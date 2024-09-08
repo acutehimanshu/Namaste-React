36b7654a0c73ef89f11848d85ea325f0316e7a17
The Cobox <acute.himanshu@gmail.com>
2024-09-08 14:42:59 +0530

    lecture 1: Inception, basic to html css js and intro of React via basics

--- App.js
+++ App.js
@@ -1,3 +1,28 @@
-const heading = React.createElement('h1', {"id":"heading"}, "Hello world with React!");
+// const heading = React.createElement('h1', {"id":"heading"}, "Hello world with React!");
+const parent = React.createElement(
+    'div',
+    {
+        id: "parent"
+    },
+    React.createElement(
+        'div',
+        {
+            id:'child'
+        },
+        [
+            React.createElement(
+                'h1',
+                {},
+                "This is h1 tag"
+            ),
+            React.createElement(
+                'h2',
+                {},
+                "This is h2 tag"
+            )
+        ]
+    )  
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(parent);
+
