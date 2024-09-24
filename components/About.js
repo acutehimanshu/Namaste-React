897c9721d22b7c8e9cfec32d0442b5999d455a13
Himanshu Sharma <acute.himanshu@gmail.com>
2024-09-24 23:14:29 +0530

    08 class based functionality, components and deep methods

--- components/About.js
+++ components/About.js
@@ -1,8 +1,34 @@
-const About = () =>{
-    return (
-        <>
-        <h1>About page</h1>
-        </>
-    )
+import { Component } from "react";
+import UsersClass from "./UsersClass";
+import Users from "./Users";
+class About  extends Component {
+    constructor(props){
+        super(props);
+        console.log("About us Parent Constructor Called");
+    }
+
+    componentDidMount(){
+        console.log("Parent Component Did Mount method Called");
+        this.timer = setInterval(() => {
+            console.log("set interval functionality");
+        }, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timer);
+    }
+    render(){
+        console.log("Parent Render Method Called");
+        
+        return(
+            <div>
+                <h1> ABout us Class Component</h1>
+                <UsersClass name={'first'} location={'First'} />
+                <Users name={'Second'} />
+                {/* <UsersClass name={'Thirt'} location={'Thirt'} /> */}
+            </div>
+        )
+    }
 }
-export default About
+
+export default About;
